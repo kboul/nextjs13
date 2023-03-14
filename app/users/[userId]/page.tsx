9cf31d0612f6691bb74d681a415e7e3e65b9d308
kboul <kos.voulgaridis@gmail.com>
2023-03-14 23:13:59 +0200

    Request dynamic metadata to change page title and description on the fly

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,17 +1,30 @@
 import getUser from "@/api/getUser";
 import getUserPost from "@/api/getUserPost";
+import { Metadata } from "next";
 import Link from "next/link";
 import { Suspense } from "react";
 
 import UserPosts from "./UserPosts";
 
-interface UserProps {
+interface Params {
   params: {
     userId: string;
   };
 }
 
-export default async function User({ params: { userId } }: UserProps) {
+export async function generateMetadata({
+  params: { userId }
+}: Params): Promise<Metadata> {
+  const userData = getUser(userId);
+  const user = await userData;
+
+  return {
+    title: user.name,
+    description: "`This is the page of ${user.name}"
+  };
+}
+
+export default async function User({ params: { userId } }: Params) {
   const userData = getUser(userId);
   const userPostData = getUserPost(userId);
 
